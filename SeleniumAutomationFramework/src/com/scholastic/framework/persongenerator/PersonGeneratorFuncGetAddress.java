c0891a8b36b24d199c23657c5ccfe9a10d0861f0
prashant <prashant@192.168.10.58>
2012-07-28 14:42:45 -0400

    Updated Java Docs + Stubs for Person Generator.

---
+++ SeleniumAutomationFramework/src/com/scholastic/framework/persongenerator/PersonGeneratorFuncGetAddress.java
@@ -0,0 +1,18 @@
+package com.scholastic.framework.persongenerator;
+
+public class PersonGeneratorFuncGetAddress extends PersonGeneratorFunc {
+
+	@Override
+	public void startFunction() {
+		try {
+			
+		} catch (Exception v_exException) {
+			this.handleException(v_exException);
+		}
+	}
+
+	public String getAddress() {
+		return "Roop Mehel, Prem Gali, Kholi Number 420 :-).";
+	}
+
+}
---
+++ SeleniumAutomationFramework/src/com/scholastic/framework/persongenerator/PersonGeneratorFuncGetName.java
@@ -0,0 +1,22 @@
+package com.scholastic.framework.persongenerator;
+
+public class PersonGeneratorFuncGetName extends PersonGeneratorFunc {
+
+	@Override
+	public void startFunction() {
+		try {
+			
+		} catch (Exception v_exException) {
+			this.handleException(v_exException);
+		}
+	}
+
+	public String getLastName() {
+		return "Keep Guessing";
+	}
+
+	public String getFirstName() {
+		return "Keep Guessing";
+	}
+
+}
